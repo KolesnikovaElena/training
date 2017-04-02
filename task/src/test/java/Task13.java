5768c8394731bcb30a163453d14972292f7cef22
KolesnikovaElena <elena.m.kolesnikova@gmail.com>
2017-04-02 16:34:27 +0300

    Исправления (удаление из корзины и еще мелочи)

--- task/src/test/java/Task13.java
+++ task/src/test/java/Task13.java
@@ -1,3 +1,5 @@
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -5,25 +7,42 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 public class Task13 extends TestBase{
 
-    @Test
-    public void cart() throws InterruptedException {
-    driver.get("http://localhost/litecart/public_html/en/");
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-     for (int i = 0; i <3; i++) {
-     List<WebElement> products = driver.findElements(By.className("product"));
-     products.get(i).click();
-     Integer quantity = Integer.parseInt(driver.findElement(By.cssSelector("#cart .quantity")).getText());
-     driver.findElement(By.cssSelector("[name=add_cart_product]")).click();
-     wait.until((WebDriver d) -> quantity != Integer.parseInt(driver.findElement(By.cssSelector("#cart .quantity")).getText()));
-     driver.findElement(By.id("logotype-wrapper"));
-    }
-  
-    }
-   
-    }
-    
+	@Test
+	public void cart()  {
+		driver.get("http://localhost/litecart/public_html/en/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		for (int i = 0; i <3; i++) {
+			List<WebElement> products = driver.findElements(By.className("product"));
+			products.get(i).click();
+			Integer quantity = Integer.parseInt(driver.findElement(By.cssSelector("#cart .quantity")).getText());
+			if(areElementsPresent(driver, By.cssSelector("td.options select"))) {
+				Select options = new Select(driver.findElement(By.cssSelector("select[name*=options]")));
+				options.selectByIndex(1);
+
+			}
+			driver.findElement(By.cssSelector("[name=add_cart_product]")).click();
+			wait.until((WebDriver d) -> quantity != Integer.parseInt(driver.findElement(By.cssSelector("#cart .quantity")).getText()));
+		}
+		driver.findElement(By.id("logotype-wrapper")).click();
+
+
+		driver.findElement(By.partialLinkText("Checkout »")).click();
+		wait.until(titleIs("Checkout | My Store"));
+		for(int product = 0; product < 3; product++) {
+			WebElement table = driver.findElement(By.cssSelector("table.dataTable.rounded-corners")) ;
+			driver.findElement(By.cssSelector("button[name=remove_cart_item]")).click();
+			wait.until(ExpectedConditions.stalenessOf(table));
+		}
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("div#content em"),
+				"There are no items in your cart."));
+	}
+
+}  
+
 
 
--- task/src/test/java/TestBase.java
+++ task/src/test/java/TestBase.java
@@ -1,5 +1,6 @@
 
 
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.logging.Level;
 
@@ -19,61 +20,73 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBase {
 
-    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
-    public WebDriver driver;
-    public WebDriverWait wait;
-    
-        public ExpectedCondition<String> thereIsWindowOtherThan (Set<String> oldWindows) {
-        return new ExpectedCondition<String>() {
-            public String apply(WebDriver driver) {
-                Set<String> handles = driver.getWindowHandles();
-                handles.removeAll(oldWindows);
-                return handles.size() > 0 ? handles.iterator().next() : null;
-            }
-        };
-    }
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+	public WebDriver driver;
+	public WebDriverWait wait;
 
-    @Before
-    public void start() {
-        if (tlDriver.get() != null) {
-            driver = tlDriver.get();
-            wait = new WebDriverWait(driver, 10);
-            return;
-        }
-        
-        DesiredCapabilities cap = DesiredCapabilities.chrome();
-        LoggingPreferences logPrefs = new LoggingPreferences();
-        logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
-        cap.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+	public ExpectedCondition<String> thereIsWindowOtherThan (Set<String> oldWindows) {
+		return new ExpectedCondition<String>() {
+			public String apply(WebDriver driver) {
+				Set<String> handles = driver.getWindowHandles();
+				handles.removeAll(oldWindows);
+				return handles.size() > 0 ? handles.iterator().next() : null;
+			}
+		};
+	}
 
-        driver = new ChromeDriver(cap);
-        tlDriver.set(driver);
-        wait = new WebDriverWait(driver, 10);
+	@Before
+	public void start() {
+		if (tlDriver.get() != null) {
+			driver = tlDriver.get();
+			wait = new WebDriverWait(driver, 10);
+			return;
+		}
 
-        Runtime.getRuntime().addShutdownHook(
-                new Thread(() -> { driver.quit(); driver = null; }));
-    }
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		LoggingPreferences logPrefs = new LoggingPreferences();
+		logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
+		cap.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
 
-    @After
-    public void stop() {
-        //driver.quit();
-        //driver = null;
-    }
-    
-    public void unhide(WebDriver driver, WebElement element) {
-    	  String script = "arguments[0].style.opacity=1;"
-    	    + "arguments[0].style['transform']='translate(0px, 0px) scale(1)';"
-    	    + "arguments[0].style['MozTransform']='translate(0px, 0px) scale(1)';"
-    	    + "arguments[0].style['WebkitTransform']='translate(0px, 0px) scale(1)';"
-    	    + "arguments[0].style['msTransform']='translate(0px, 0px) scale(1)';"
-    	    + "arguments[0].style['OTransform']='translate(0px, 0px) scale(1)';"
-    	    + "return true;";
-    	  ((JavascriptExecutor) driver).executeScript(script, element);
-    	}
+		driver = new ChromeDriver(cap);
+		tlDriver.set(driver);
+		wait = new WebDriverWait(driver, 10);
 
-    	public void attachFile(WebDriver driver, By locator, String file) {
-    	  WebElement input = driver.findElement(locator);
-    	  unhide(driver, input);
-    	  input.sendKeys(file);
-    	}
+		Runtime.getRuntime().addShutdownHook(
+				new Thread(() -> { driver.quit(); driver = null; }));
+	}
+
+	@After
+	public void stop() {
+		//driver.quit();
+		//driver = null;
+	}
+
+	public void unhide(WebDriver driver, WebElement element) {
+		String script = "arguments[0].style.opacity=1;"
+				+ "arguments[0].style['transform']='translate(0px, 0px) scale(1)';"
+				+ "arguments[0].style['MozTransform']='translate(0px, 0px) scale(1)';"
+				+ "arguments[0].style['WebkitTransform']='translate(0px, 0px) scale(1)';"
+				+ "arguments[0].style['msTransform']='translate(0px, 0px) scale(1)';"
+				+ "arguments[0].style['OTransform']='translate(0px, 0px) scale(1)';"
+				+ "return true;";
+		((JavascriptExecutor) driver).executeScript(script, element);
+	}
+
+	public void attachFile(WebDriver driver, By locator, String file) {
+		WebElement input = driver.findElement(locator);
+		unhide(driver, input);
+		input.sendKeys(file);
+	}
+
+	protected boolean isElementPresent(WebDriver driver, By locator) {
+		try {
+			driver.findElement(locator);
+			return true;
+		} catch (NoSuchElementException ex) {
+			return false;
+		}
+	}
+	public  boolean areElementsPresent(WebDriver driver, By locator) {
+		return driver.findElements(locator).size() > 0;
+	}
 }
